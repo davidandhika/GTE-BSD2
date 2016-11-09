88e21388103c487398b112fe994f6237661b92f0
kurro.xp@gmail.com <kurro.xp@gmail.com>
2016-11-09 23:11:03 +0700

    Search Transaksi Masuk

---
+++ Gudang Toko Elektronik/Gudang Toko Elektronik/Client/Search_Masuk.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Search_Masuk
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Search_Masuk))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.close_btn = New System.Windows.Forms.Button()
+        Me.search_btn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(10, 95)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(579, 240)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'close_btn
+        '
+        Me.close_btn.BackgroundImage = CType(resources.GetObject("close_btn.BackgroundImage"), System.Drawing.Image)
+        Me.close_btn.Location = New System.Drawing.Point(566, 56)
+        Me.close_btn.Name = "close_btn"
+        Me.close_btn.Size = New System.Drawing.Size(24, 24)
+        Me.close_btn.TabIndex = 11
+        Me.close_btn.UseVisualStyleBackColor = True
+        '
+        'search_btn
+        '
+        Me.search_btn.BackgroundImage = CType(resources.GetObject("search_btn.BackgroundImage"), System.Drawing.Image)
+        Me.search_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.search_btn.Location = New System.Drawing.Point(407, 58)
+        Me.search_btn.Name = "search_btn"
+        Me.search_btn.Size = New System.Drawing.Size(44, 29)
+        Me.search_btn.TabIndex = 10
+        Me.search_btn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(162, 59)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(238, 27)
+        Me.TextBox1.TabIndex = 9
+        Me.TextBox1.Text = "Kode Barang"
+        '
+        'Search_Masuk
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(601, 347)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.close_btn)
+        Me.Controls.Add(Me.search_btn)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Search_Masuk"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Search_Masuk"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents close_btn As System.Windows.Forms.Button
+    Friend WithEvents search_btn As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+End Class
---
+++ Gudang Toko Elektronik/Gudang Toko Elektronik/Client/Search_Masuk.vb
@@ -0,0 +1,41 @@
+﻿Imports Oracle.DataAccess.Client
+
+Public Class Search_Masuk
+    Dim ds As New DataSet
+    Dim da As OracleDataAdapter
+    Private Sub search_btn_Click(sender As Object, e As EventArgs) Handles search_btn.Click
+        Try
+            Dim cmd As New OracleCommand("select * from s_barang where kdbarang='" & TextBox1.Text & "'", conn)
+            da = New OracleDataAdapter(cmd)
+            da.Fill(ds, "s_barang")
+            DataGridView1.DataSource = ds.Tables(0)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellDoubleClick
+        Dim barisdgv As Integer = e.RowIndex
+        If barisdgv >= 0 Then
+            FormMasuk.kdbarang_txt.Text = DataGridView1.Item(0, barisdgv).Value
+            FormMasuk.nmBarang_txt.Text = DataGridView1.Item(1, barisdgv).Value
+            FormMasuk.spek_txt.Text = DataGridView1.Item(2, barisdgv).Value
+        End If
+        Me.Close()
+    End Sub
+
+    Private Sub close_btn_Click(sender As Object, e As EventArgs) Handles close_btn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Search_Masuk_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim cmd As New OracleCommand("select * from s_barang", conn)
+            da = New OracleDataAdapter(cmd)
+            da.Fill(ds, "s_barang")
+            DataGridView1.DataSource = ds.Tables(0)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
