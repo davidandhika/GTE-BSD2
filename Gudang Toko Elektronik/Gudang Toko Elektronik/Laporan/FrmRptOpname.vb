2e7591807e1d093f6571d1b5cc950e3a88018827
Redloki <redloki1@gmail.com>
2016-11-18 20:22:34 +0700

    laporan&design_Opname

---
+++ Gudang Toko Elektronik/Gudang Toko Elektronik/Laporan/FrmRptOpname.vb
@@ -0,0 +1,8 @@
+﻿Imports CrystalDecisions.CrystalReports.Engine
+Public Class FrmRptOpname
+    Private Sub FrmRptOpname_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim rpt As New ReportDocument
+        rpt.Load("path/FrmRptOpname.vb")
+        CrystalReportViewer1.ReportSource = rpt
+    End Sub
+End Class
